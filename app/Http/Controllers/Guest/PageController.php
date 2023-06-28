23b389f7e28b30f14170916b86388c422bee38dc
IreneCaiazzo <irene.caiazzo707@gmail.com>
2023-06-28 09:19:37 +0200

    add routes and page controller

---
+++ app/Http/Controllers/Guest/PageController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index()
+    {
+        return view('welcome');
+    }
+
+    public function about()
+    {
+        return view('about');
+    }
+
+    public function movies()
+    {
+        return view('movies');
+    }
+}
